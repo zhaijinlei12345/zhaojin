52b7aae5190a886d6b8f3e3c8feef01138771be1
zhaijinlei12345 <1152005456@qq.com>
2018-03-18 23:04:59 +0800

    前端娱乐app开发3完善

--- vue-cli/vue-project/src/components/music/MusicPlayer.vue
+++ vue-cli/vue-project/src/components/music/MusicPlayer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class='player'>
         <a-player :music="musicList" :narrow="false" :autoplay="true"
-        :mutex="true" theme="#ff0000" listMaxheight="513px" v-if="isShow">
+        :mutex="true"  v-if="isShow">
         </a-player>
         <!-- {{$route.params.id}} -->
     </div>
@@ -9,17 +9,10 @@
 <script>
 import Axios from "axios"
 import APlayer from "vue-aplayer"
- export default {
+export default {
   data(){
       return {
           musicData: [],
-//          music: {
-//          "title":"刚好遇见你",
-//          "author":"李玉刚",
-//          "src": "http://m2.music.126.net/qv3RI4K7ABKJ0TyAdb3taw==/3250156397064860.mp3",
-//           "pic": "http://p4.music.126.net/lDyytkTaPYVTb1Vpide6AA==/18591642115187138.jpg",
-//           "lrc":"刚好遇见你-李玉刚.lrc"
-//       },
           musicList: [],
           isShow   : false
       }
@@ -37,14 +30,14 @@ import APlayer from "vue-aplayer"
                   obj.lrc    = "/static/"+this.musicData[i].lrc;
                   this.musicList.push(obj);
           }
-          console.log(this.musicList);
           this.isShow = true;
+          console.log(this.musicList);
       });
   },
   components:{
       APlayer
   }
-
+  
 }
 </script>
 <style>
