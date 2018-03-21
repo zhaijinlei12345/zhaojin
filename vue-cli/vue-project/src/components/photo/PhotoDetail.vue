9013eb180ceff4f224b9d8ab6c68e2f00e4f7adf
zhaijinlei12345 <1152005456@qq.com>
2018-03-21 23:26:04 +0800

    前端娱乐app开发6

--- vue-cli/vue-project/src/components/photo/PhotoDetail.vue
+++ vue-cli/vue-project/src/components/photo/PhotoDetail.vue
@@ -2,9 +2,8 @@
 <div class="detail">
   <common-header title="Photo" bgColor="#c20c0c" nav="<"></common-header>
   <!--{{$route.params.index}}-->
-  <div class="photo-detail">
-    <img :src="photoData[index].src" alt="">
-  </div>
+  <v-touch class="photo-detail" :style="{background:bg}" @swipeleft="left"  @swiperight="right">
+  </v-touch>
 </div>
 </template>
 <script>
@@ -17,20 +16,47 @@
       return{
           index:this.$route.params.index
       }
-    },
+    }, 
     computed:{
+        bg(){
+            return "url("+this.$store.state.photoData[this.index].src+") no-repeat center / contain #000"
+        },
       ...mapState(["photoData"])
+    },
+    methods: {
+      left(){
+            this.index++;
+        if(this.$store.state.photoData.length==this.index){
+            this.index=0;
+        }
+         
+        } ,
+         
+     
+      right(){
+          this.index--;
+         if(this.index=='-1'){
+           this.index=(this.$store.state.photoData.length-1);
+         }
+         
+         
+      }
+
     },
     components:{
       CommonHeader
-    }
+    } 
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
   .photo-detail{
-    margin-top: 3rem;
+     position: absolute;
+     top: 1rem;
+     bottom: 0;
+     width: 100%;
+
   }
 
 </style>
