e015abf166ba4db22915067ae4162d7b5e85c344
zhaijinlei12345 <1152005456@qq.com>
2018-04-09 23:08:59 +0800

    网站设计_img_top

--- 网站/T/js/indexContatiner.js
+++ 网站/T/js/indexContatiner.js
@@ -1,6 +1,22 @@
 (function () {
     $("#navul li").on("click", function () {
         $(this).addClass("selected").siblings().removeClass("selected");
-    })
+    });
+
+    $(".gotop").on("click", function(){
+        var target;
+        if(document.documentElement.scrollTop){//$("html).scrollTop()
+            target = $("html");
+        }else{
+            target = $("body");
+        }
+        var timer = setInterval(function(){
+            var iScrollTop = target.scrollTop();
+            if(iScrollTop <= 0){
+                clearInterval(timer);
+            }
+            target.scrollTop(iScrollTop-=50);
+        }, 500);
+    });
 
 })();
