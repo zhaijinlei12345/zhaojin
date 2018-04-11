b37990b98c7b7bda75eace885d2e30bca4e280a8
zhaijinlei12345 <1152005456@qq.com>
2018-04-11 23:13:54 +0800

    js基础原理复习_1

--- 网站/T/js/indexContatiner.js
+++ 网站/T/js/indexContatiner.js
@@ -20,6 +20,20 @@
             target.scrollTop(iScrollTop-=50);
         }, 500);
     });
+   $(".nav_home").on("click",function () {
+       var ta;
+       if(document.documentElement.scrollTop){
+           ta=$("#Home");
+       }
+       var timer = setInterval(function(){
+           var iScrollTop = ta.scrollTop();
+           if(iScrollTop <= 0){
+               clearInterval(timer);
+           }
+           target.scrollTop(iScrollTop-=50);
+       }, 500);
+
+   })
 
 
 })();
