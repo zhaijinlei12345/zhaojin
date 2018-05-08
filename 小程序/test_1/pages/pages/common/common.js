eb78306cdb3fa3282116f9595d681082ebc9dbac
zhaijinlei12345 <1152005456@qq.com>
2018-05-08 23:07:33 +0800

    小程序测试_3

---
+++ 小程序/test_1/pages/pages/common/common.js
@@ -0,0 +1,10 @@
+require("a.js");
+function sayHello(name) {
+  console.log(`Hello ${name} !`)
+}
+function sayGoodbye(name) {
+  console.log(`Goodbye ${name} !`)
+}
+
+module.exports.sayHello = sayHello
+module.exports.sayGoodbye = sayGoodbye
